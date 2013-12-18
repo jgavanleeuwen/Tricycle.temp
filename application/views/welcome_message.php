407dd04aa3063e716e5eb7feaa521fd11723e974
jgavanleeuwen <jgavanleeuwen@outlook.com>
2013-12-18 10:13:08 +0100

    First commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Tri-cycle.co | Coming soon</title>
+
+	<!-- Bootstrap -->
+	<link href="public/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+	<!-- Webfonts -->
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+
+
+	<!-- Style -->
+	<link href="public/css/style.css" rel="stylesheet" media="screen">
+
+</head>
+<body>
+
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col">
+				<center>
+					<h2><span class="lobster">Tricycle</span><br /><span> is coming soon</span></h2>
+				</center>
+			</div>
+		</div>
+	</div>
+
+	<!-- Bootstrap core JavaScript
+  ================================================== -->
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+  <script src="public/js/bootstrap.min.js"></script>
+
+</body>
+</html>
